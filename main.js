97c37013ce0747e4a74049fb41e7bec9aba7fb03
sorrel012 <wain1719@gmail.com>
2023-05-08 01:12:44 +0900

    feat: Connect project detail files

---
+++ main.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', function() {
+    window.location.href = 'project3_java.html';
+});
+btn1.addEventListener('click', function() {
+    window.location.href = 'project4_oracle.html';
+});
